e2e43f38225631f7c1813dfefeb202b8909b3fc2
Vova <krahs123@mail.ru>
2022-05-17 17:48:28 +0300

    arr training

---
+++ indx.js
@@ -0,0 +1,29 @@
+/* const family = ['Peter', 'Ann', 'Alex', 'Linda'];
+
+function showFamily(arr) {
+        let str = `Семья состоит из: ${arr}`;
+
+
+
+        str = str.toLowerCase();
+
+        return str;
+}
+
+showFamily(family);
+ */
+
+const favoriteCities = ['liSBon', 'ROME', 'miLan', 'Dublin'];
+
+function standardizeStrings(arr) {
+
+ let str = `${arr}`;
+
+ str = str.toLowerCase();
+
+return str;
+
+}
+
+standardizeStrings(favoriteCities);
+console.log(standardizeStrings(favoriteCities));
--- script.js
+++ script.js
@@ -1,100 +1,28 @@
-'use strict';
-
-let a = 5,
-    b = a;
-
-    b = b + 5;
-
-    console.log(b);
-    console.log(a);
-
-
-    const obj = {
-        a: 5,
-        b: 1
-    };
-/* 
-    const copy = obj; // Ссылка
-
-    copy.a = 10;
-
-    console.log(copy);
-    console.log(obj); */
-
-    function copy(mainObj) {
-        let objCopy = {};
-
-
-        let key;
-
-        for (key in mainObj) {
-            objCopy[key] = mainObj[key];
-        }
-
-        return objCopy; //Чтобы можно было с ним работать
+const personalPlanPeter = {
+    name: "Peter",
+    age: "29",
+    skills: {
+        languages: ['ru', 'eng'],
+        programmingLangs: {
+            js: '20%',
+            php: '10%'
+        },
+        exp: '1 month'
     }
+};
 
-    const numbers = {
-        a: 2,
-        b: 5,
-        c: {
-            x: 7,
-            y: 4
-        }
-    };
+function showExperience(plan) {
+const nameObj = plan.name;
+const expObj = plan.skills.exp;
+const someStr = plan.skills.languages[0];
 
-    const newNumbers = copy(numbers); //передаем в функцию аргумента массива(любого)
+let str = `Меня зовут  ${nameObj}, мой опыт ${expObj} + ${someStr}`;
+str = str.toUpperCase();
 
-    newNumbers.a = 10;
-    newNumbers.c.x = 10;
+return str;
 
-    console.log(newNumbers);
-    console.log(numbers);
-
-    const add = {
-        d: 17,
-        e: 20
-    };
-const clone = Object.assign({}, add);//метод = закинуть обьект в обьект,в пустой или в существующий,при этом он независимый
-
-clone.d = 20;
-
-/* console.log(add);
-console.log(clone);
- */
-
-const oldArray = ['a', 'b', 'c'];
-const newArray = oldArray.slice();  //создает обособленную копию
-newArray[1] = 'dsdsdsdsd';
-console.log(newArray);
-console.log(oldArray);
-
-const video = ['youtube', 'vimeo', 'rutbe'],
-        blogs = ['wordpress', 'livejournal', 'blogger'],
-
-        internet = [...video, ...blogs, 'vk', 'facebook'];//склейка массивов в один
-
-        console.log(internet);
-
-        function log(a, b, c) {
-            console.log(a);
-            console.log(b);
-            console.log(c);
-
-        }
-
-        const num = [2, 5, 7];
-
-        log(...num);//спред оператор
+}
 
 
-        const array = ["a", "b"];
+showExperience(personalPlanPeter);
 
-        const newAarray = [...array];
-
-        const q = {
-            one: 1,
-            two: 2
-        };
-
-        const newObj = {...q};
